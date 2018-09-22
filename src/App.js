bbd0fe2b2ddcddfe8248f1669c425c5ed7f08648
Saepuloh <saepuloh_februari@gmail.com>
2018-09-22 16:46:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ class App extends Component {
         <div className="header">
           <div className="h-1">
             <h1>facebook</h1>
-            <from className="formh" action="index.js" method="post">
+            <form className="formh" action="index.js" method="post">
               <label for="">Email atau Telepon</label>
               <label for="">Kata Sandi</label>
               <input id='txtb' type="text" placeholder="" name=" uname " required />
               <input id='txtb' type="password" placeholder="" name=" psw " required />
               <button id='btn'>Masuk</button>
               <a href="#">Lupa akun?</a>
-            </from>
+            </form>
           </div>
         </div>
         <div className="cent">
           <div className="c1">
             <h2>Facebook membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda.</h2>
             <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yc/r/GwFs3_KxNjS.png" alt=""></img>
-            <from className="Daftar" action="index.js" method="post">
+            <form className="Daftar" action="index.js" method="post">
               <h1>Daftar</h1>
               <p>Gratis, sampai kapan pun.</p>
               <input id='txt1' type="text" placeholder="Nama depan" name=" uname " required />
@@ -101,7 +101,7 @@ class App extends Component {
               </span>
               Dengan mengklik Daftar, Anda menyetujui <a href="#">Ketentuan, Kebijakan Data </a> dan <a href="#">Kebijakan Cookie kami</a>.Anda akan menerima Notifikasi SMS dari Facebook dan dapat menolaknya kapan saja
               <br></br><input id='btn2' type="button" name="" value="Daftar"></input>
-            </from>
+            </form>
           </div>
         </div>
       </div>
